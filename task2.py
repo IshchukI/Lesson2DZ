e6222427f72e03b82b2e672b0cb2429dec38797a
I_Ishchuk <yaroslavischuck@gmail.com>
2022-01-25 19:37:15 +0200

    task 2 done

--- task2.py
+++ task2.py
@@ -1,1 +1,12 @@
-print("test git sss")
+number = int(input("Введите трехзначное число: "))
+LOW_LIMIT = 100
+HI_LIMIT = 999
+
+if (number < LOW_LIMIT or number > HI_LIMIT):
+    print("Необходимо ввести трехзначное число")
+else:
+    firstDigitOfNumber = number % 10
+    secondDigitOfNumber = int((number % 100) / 10)
+    thirdDigitOfNumber = int(number / 100)
+    sum = firstDigitOfNumber + secondDigitOfNumber + thirdDigitOfNumber
+    print(f"{firstDigitOfNumber} + {secondDigitOfNumber} + {thirdDigitOfNumber} = {sum}")
